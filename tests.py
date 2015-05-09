49a6bec7c629e5a84e07c55301f2447f890bad4c
Shaumik <sdaityari@gmail.com>
2015-05-10 00:51:36 +0530

    Added more tests

--- tests.py
+++ tests.py
@@ -8,5 +8,14 @@ class TestsForAddFunction(unittest.TestCase):
         result = add_two_numbers(0, 0)
         self.assertEqual(0, result)
 
+    def test_both_positive(self):
+        result = add_two_numbers(5, 7)
+        self.assertEqual(12, result)
+
+    def test_both_negative(self):
+        result = add_two_numbers(-5, -7)
+        self.assertEqual(-12, result)
+
+
 if __name__ == '__main__':
     unittest.main()
